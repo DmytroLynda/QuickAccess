8fcdbbc51ed5eb035048d57e6feb3a3a28f37a5f
adnylortym <adnylortym@gmail.com>
2020-12-23 15:06:32 +0100

    Add http server project

---
+++ Server/HttpServer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class HttpServer : IServer
+    {
+        private readonly ILogger<HttpServer> _logger;
+
+        public HttpServer(ILogger<HttpServer> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
